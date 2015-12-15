b3304a393c803e6743c26fed7f1f0df15a3f8ef0
Daniel Adler <weston.adler@gmail.com>
2015-12-14 17:13:30 -0800

    refactor basic styling with Bootstrap

--- calc-daniel.js
+++ calc-daniel.js
@@ -1,20 +1,9 @@
 window.onload = function () {
 
-  // styling
-  document.body.style.backgroundColor = "rgba(100, 163, 203, 0.35)"
-  document.body.style.width = "800px"
-  document.body.style.minWidth = "600px"
-  document.body.style.maxWidth = "800px"
-  document.body.style.margin = "0 auto"
-  document.body.style.fontFamily = "Helvetica Neue"
-
   // set up page elements
   var answer = document.getElementById("answer");
   answer.innerHTML = "0";
-  answer.style.border      = "1px solid black";
-  answer.style.width       = "100%";
-  answer.style.paddingLeft = "10px";
-  answer.style.fontSize    = "40px";
+
 
   // events
 
@@ -24,8 +13,8 @@ window.onload = function () {
         var inputX = document.getElementById("inputX").value;
         var op     = document.getElementById("operator").value;
         var inputY = document.getElementById("inputY").value;
-        console.log(inputX);
-        console.log(inputY);
+        // console.log(inputX);
+        // console.log(inputY);
 
         var array  = [ ]
         array.push(inputX, op, inputY);
@@ -33,7 +22,7 @@ window.onload = function () {
       });
 
       // IN PROGRESS: put math into own function; call function on click event
-      // function doCalcMath() {
+      // function doMath() {
       //   array.push(inputX, op, inputY);
       //   answer.innerHTML = eval(array[0] + array[1] + array[2]);
       // // }
